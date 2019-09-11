cb577d57589d1811afe206ad7c05702b334cbb5b
Younghyun Cho <cho242@purdue.edu>
2019-09-11 19:14:07 -0400

    back pressed button added

--- app/src/main/java/com/cs407/team15/redstone/MainActivity.kt
+++ app/src/main/java/com/cs407/team15/redstone/MainActivity.kt
@@ -13,6 +13,16 @@ import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.cs407.team15.redstone.ui.aboutpurdue.AboutPurdueFragment
+import com.cs407.team15.redstone.ui.ar.ARFragment
+import com.cs407.team15.redstone.ui.home.HomeFragment
+import com.cs407.team15.redstone.ui.profile.ProfileFragment
+import com.cs407.team15.redstone.ui.tour.TourFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,9 +39,12 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
+
         val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
         val navView: NavigationView = findViewById(R.id.nav_view)
+
         val navController = findNavController(R.id.nav_host_fragment)
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
@@ -50,5 +63,14 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 }
