607460f25faf78946589c2662ece43a6e6618940
sklarqui <sklarqui@purdue.edu>
2019-09-29 16:42:51 -0400

    Added map

--- app/src/main/java/com/cs407/team15/redstone/ui/tour/TourFragment.kt
+++ app/src/main/java/com/cs407/team15/redstone/ui/tour/TourFragment.kt
@@ -9,11 +9,18 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.cs407.team15.redstone.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class TourFragment : Fragment() {
+
+class TourFragment : Fragment(), OnMapReadyCallback{
 
     private lateinit var tourViewModel: TourViewModel
-
+    private  lateinit var mMap: GoogleMap
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,10 +29,25 @@ class TourFragment : Fragment() {
         tourViewModel =
             ViewModelProviders.of(this).get(TourViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_tour, container, false)
-        val textView: TextView = root.findViewById(R.id.text_tour)
-        tourViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        //val textView: TextView = root.findViewById(R.id.text_tour)
+        //tourViewModel.text.observe(this, Observer {
+          //  textView.text = it
+            //val mapFragment = childFragmentManager.findFragmentById(R.id.tour_map) as SupportMapFragment
+            //mapFragment.getMapAsync(this)
+        //})
+
+        val mapFragment = childFragmentManager.findFragmentById(R.id.tour_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
         return root
+    }
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.setMaxZoomPreference(6.0f)
+        mMap.uiSettings.isZoomControlsEnabled=true
+        val purdue = LatLng(40.4237,-86.9212)
+        mMap.addMarker(MarkerOptions().position(purdue).title("PURDUEEE"))
+        mMap.animateCamera(CameraUpdateFactory.zoomIn())
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(purdue))
     }
 }
--- app/src/main/java/com/cs407/team15/redstone/ui/tour/TourViewModel.kt
+++ app/src/main/java/com/cs407/team15/redstone/ui/tour/TourViewModel.kt
@@ -3,8 +3,9 @@ package com.cs407.team15.redstone.ui.tour
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.android.gms.maps.GoogleMap
 
-class TourViewModel : ViewModel() {
+abstract class TourViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is Tour Fragment"
