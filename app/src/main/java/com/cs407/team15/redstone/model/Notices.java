e090fd5d35db7360822dd3355420f327ef560c9b
Jungyoon Yu <jungy24@gmail.com>
2019-10-03 16:24:04 -0400

    Completed u.s. 17

--- app/src/main/java/com/cs407/team15/redstone/model/Notices.java
+++ app/src/main/java/com/cs407/team15/redstone/model/Notices.java
@@ -1,6 +1,9 @@
 package com.cs407.team15.redstone.model;
 
-public class Notices {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Notices implements Parcelable {
     private String writer;
     private String title;
     private String content;
@@ -14,6 +17,28 @@ public class Notices {
         this.date = date;
         this.notice_id = notice_id;
     }
+
+    public Notices() {}
+
+    protected Notices(Parcel in) {
+        writer = in.readString();
+        title = in.readString();
+        content = in.readString();
+        date = in.readString();
+        notice_id = in.readInt();
+    }
+
+    public static final Creator<Notices> CREATOR = new Creator<Notices>() {
+        @Override
+        public Notices createFromParcel(Parcel in) {
+            return new Notices(in);
+        }
+
+        @Override
+        public Notices[] newArray(int size) {
+            return new Notices[size];
+        }
+    };
 
     public String getWriter() {
         return writer;
@@ -53,6 +78,20 @@ public class Notices {
 
     public void setNotice_id(int notice_id) {
         this.notice_id = notice_id;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(writer);
+        parcel.writeString(title);
+        parcel.writeString(content);
+        parcel.writeString(date);
+        parcel.writeInt(notice_id);
     }
 }
 
--- app/src/main/java/com/cs407/team15/redstone/ui/home/NoticesAdapter.java
+++ app/src/main/java/com/cs407/team15/redstone/ui/home/NoticesAdapter.java
@@ -25,15 +25,18 @@ public class NoticesAdapter extends RecyclerView.Adapter<NoticesAdapter.ViewHold
     private String TAG = getClass().getName();
     private ArrayList<Notices> noticeList;
 
-    public NoticesAdapter(Context context, ArrayList<Notices> noticeList) {
+    private OnNoticeListener mOnNoticeListener;
+
+    public NoticesAdapter(Context context, ArrayList<Notices> noticeList, OnNoticeListener onNoticeListener) {
         this.context = context;
         this.noticeList = noticeList;
+        this.mOnNoticeListener = onNoticeListener;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.notice_recycle_view_item,null);
-        return new ViewHolder(v);
+        return new ViewHolder(v, mOnNoticeListener);
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -55,22 +58,35 @@ public class NoticesAdapter extends RecyclerView.Adapter<NoticesAdapter.ViewHold
     }
 
     /** item layout **/
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         TextView tv_title;
         TextView tv_date;
         TextView tv_content;
         TextView tv_writer;
         CardView cv;
 
-        public ViewHolder(View v) {
+        OnNoticeListener onNoticeListener;
+
+        public ViewHolder(View v, OnNoticeListener onNoticeListener) {
             super(v);
             tv_title = (TextView) v.findViewById(R.id.tv_title);
             tv_date = (TextView) v.findViewById(R.id.tv_date);
             tv_content = (TextView) v.findViewById(R.id.tv_content);
             tv_writer = (TextView) v.findViewById(R.id.tv_writer);
             cv = (CardView) v.findViewById(R.id.cv);
+
+            this.onNoticeListener = onNoticeListener;
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            onNoticeListener.onNoticeClick(getAdapterPosition());
         }
     }
 
+    public interface OnNoticeListener{
+        void onNoticeClick(int position);
+    }
 
 }
