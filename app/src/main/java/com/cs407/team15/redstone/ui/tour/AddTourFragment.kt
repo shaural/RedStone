42bf79c958169f52d3b218bd8bede3d331688c99
Victoria <voldson@purdue.edu>
2019-10-20 09:12:17 -0400

    Basic addTour fragment added

--- app/src/main/java/com/cs407/team15/redstone/ui/tour/AddTourFragment.kt
+++ app/src/main/java/com/cs407/team15/redstone/ui/tour/AddTourFragment.kt
@@ -1,2 +1,19 @@
 package com.cs407.team15.redstone.ui.tour
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cs407.team15.redstone.R
+
+class AddTourFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_addtour, container, false)
+    }
+}
