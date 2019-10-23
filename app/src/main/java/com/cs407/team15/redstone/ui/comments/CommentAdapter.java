f78f1f6145d9c1b64aa3068b8aa06a14c8251243
Younghyun Cho <cho242@purdue.edu>
2019-10-22 20:15:19 -0400

    displaying comments start

---
+++ app/src/main/java/com/cs407/team15/redstone/ui/comments/CommentAdapter.java
@@ -0,0 +1,82 @@
+package com.cs407.team15.redstone.ui.comments;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.cs407.team15.redstone.R;
+import com.cs407.team15.redstone.model.Comment;
+import com.cs407.team15.redstone.ui.home.NoticesAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
+
+public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ImageViewHolder> {
+
+    private Context mContext;
+    private List<Comment> mComment;
+    private String postid;
+
+    private FirebaseUser firebaseUser;
+    private FirebaseFirestore db;
+    private CollectionReference userref;
+    public static final String COLLECTION_NAME_KEY = "users";
+
+
+
+    public CommentAdapter(Context context, List<Comment> comments, String postid){
+        mContext = context;
+        mComment = comments;
+        this.postid = postid;
+    }
+
+    @NonNull
+    @Override
+    public CommentAdapter.ImageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.comment_item, parent, false);
+        return new CommentAdapter.ImageViewHolder(view);
+    }
+
+
+
+
+    @Override
+    public int getItemCount() {
+        return mComment.size();
+    }
+
+    public class ImageViewHolder extends RecyclerView.ViewHolder {
+
+        public ImageView image_profile;
+        public TextView username, comment;
+
+        public ImageViewHolder(View itemView) {
+            super(itemView);
+
+            image_profile = itemView.findViewById(R.id.image_profile);
+            username = itemView.findViewById(R.id.username);
+            comment = itemView.findViewById(R.id.comment);
+        }
+    }
+
+    private void getUserInfo(String email, final ImageView imageView, final TextView username, String publisherid){
+        DocumentReference docRef = db.collection("users").document(email);
+
+    }
+
+}
