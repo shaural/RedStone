be1cf5b06c90aa40f216b7d49a11a860ec570045
Younghyun Cho <cho242@purdue.edu>
2019-10-22 21:55:46 -0400

    display/remove comments added

--- app/src/main/java/com/cs407/team15/redstone/model/Comment.java
+++ app/src/main/java/com/cs407/team15/redstone/model/Comment.java
@@ -4,11 +4,21 @@ public class Comment {
     private String comment;
     private String publisher;
     private String commentid;
+    private String path;
 
-    public Comment(String comment, String publisher, String commentid) {
+    public Comment(String comment, String publisher, String commentid, String path) {
         this.comment = comment;
         this.publisher = publisher;
         this.commentid = commentid;
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
     }
 
     public Comment() {
--- app/src/main/java/com/cs407/team15/redstone/ui/comments/CommentsActivity.java
+++ app/src/main/java/com/cs407/team15/redstone/ui/comments/CommentsActivity.java
@@ -2,6 +2,8 @@ package com.cs407.team15.redstone.ui.comments;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,20 +11,34 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 import com.cs407.team15.redstone.R;
+import com.cs407.team15.redstone.model.Comment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class CommentsActivity extends AppCompatActivity {
     String TAG = getClass().toString();
+
+    private RecyclerView recyclerView;
+    private CommentAdapter commentAdapter;
+    private List<Comment> commentList;
+    private ProgressBar progressBar;
+
+
     EditText addcomment;
     ImageView image_profile;
     TextView post;
@@ -50,12 +66,6 @@ public class CommentsActivity extends AppCompatActivity {
             }
         });
 
-        addcomment = findViewById(R.id.add_comment);
-        image_profile = findViewById(R.id.image_profile);
-        post = findViewById(R.id.post);
-
-        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-
         Intent intent = getIntent();
         postid = intent.getStringExtra("postid");
         publisherid = intent.getStringExtra("publisherid");
@@ -63,6 +73,22 @@ public class CommentsActivity extends AppCompatActivity {
         Log.e(TAG, "PATH:" + path);
         Log.e(TAG, "Author:"+ publisherid);
         Log.e(TAG, "Postid: "+ postid);
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        commentList = new ArrayList<>();
+        commentAdapter = new CommentAdapter(this, commentList, postid, path);
+        recyclerView.setAdapter(commentAdapter);
+
+
+        post = findViewById(R.id.post);
+        addcomment = findViewById(R.id.add_comment);
+        image_profile = findViewById(R.id.image_profile);
+        progressBar = findViewById(R.id.comment_loading);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
 
         post.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,16 +100,50 @@ public class CommentsActivity extends AppCompatActivity {
                 }
             }
         });
+
+        progressBar.setVisibility(View.VISIBLE);
+        readComments();
     }
 
     private  void addComment() {
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Comments").child(path).child(postid);
 
+        String commentid = reference.push().getKey();
+
         HashMap<String, Object> hashMap = new HashMap<>();
         hashMap.put("comment", addcomment.getText().toString());
-        hashMap.put("publisherid", firebaseUser.getUid());
+        hashMap.put("publisher", firebaseUser.getEmail());
+        hashMap.put("path", path);
+        hashMap.put("commentid", commentid);
 
-        reference.push().setValue(hashMap);
+        reference.child(commentid).setValue(hashMap);
+
         addcomment.setText("");
+    }
+
+    private void readComments(){
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Comments").child(path).child(postid);
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                progressBar.setVisibility(View.VISIBLE);
+                commentList.clear();
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    Comment comment = snapshot.getValue(Comment.class);
+                    commentList.add(comment);
+                }
+
+                commentAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+
     }
 }
