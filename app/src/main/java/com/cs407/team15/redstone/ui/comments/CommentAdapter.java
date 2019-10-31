24fc332c7354846148e4dc57f88552165338c53e
sklarqui <sklarqui@purdue.edu>
2019-10-31 17:54:49 -0400

    can dilike comments

--- app/src/main/java/com/cs407/team15/redstone/ui/comments/CommentAdapter.java
+++ app/src/main/java/com/cs407/team15/redstone/ui/comments/CommentAdapter.java
@@ -179,7 +179,9 @@ public class CommentAdapter extends RecyclerView.Adapter<CommentAdapter.ImageVie
             score = itemView.findViewById(R.id.comment_score);
 
             up_vote = itemView.findViewById(R.id.up_vote_comment);
+            down_vote = itemView.findViewById(R.id.down_vote_comment);
             score = itemView.findViewById(R.id.comment_score);
+
         }
     }
 
