d0e0ca6515c36485386278f5949dca613697adad
Younghyun Cho <cho242@purdue.edu>
2019-11-14 18:05:20 -0500

    Admin Page access button

---
+++ app/src/main/java/com/cs407/team15/redstone/ui/adminpage/adminActivity.java
@@ -0,0 +1,16 @@
+package com.cs407.team15.redstone.ui.adminpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cs407.team15.redstone.R;
+
+public class adminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
